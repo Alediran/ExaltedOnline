d936be6cdc7ad98152bf0bccadf8fb4b25ba7609
Alediran <55855331+Alediran@users.noreply.github.com>
2020-06-28 16:15:58 -0300

    Updating Angular

--- ExaltedOnlineWeb/ClientApp/src/app/circular-check/circular-check.component.ts
+++ ExaltedOnlineWeb/ClientApp/src/app/circular-check/circular-check.component.ts
@@ -1,32 +1,30 @@
-import { Component, OnInit, Input, HostListener } from '@angular/core';
+import { Component, OnInit, Input, HostListener } from "@angular/core";
 
 @Component({
-  selector: 'app-circular-check',
-  templateUrl: './circular-check.component.html',
-  styleUrls: ['./circular-check.component.scss']
+  selector: "app-circular-check",
+  templateUrl: "./circular-check.component.html",
+  styleUrls: ["./circular-check.component.scss"],
 })
 export class CircularCheckComponent implements OnInit {
-    @Input() Position: Number = 0;
-    @Input() Name: string;
-    @Input() Checked: boolean;
-    Id: string;
-    check: HTMLInputElement;
+  @Input() Position: Number = 0;
+  @Input() Name: string;
+  @Input() Checked: boolean;
+  Id: string;
+  check: HTMLInputElement;
 
-    constructor() {
+  constructor() {
+    this.Checked = false;
+  }
 
-        this.Checked = false;
-    }
+  ngOnInit() {}
 
-    ngOnInit() {
-        
-    }
+  ngAfterContentChecked() {
+    this.Id = this.Name + "_" + this.Position;
 
-    ngAfterContentChecked() {
-        this.Id = this.Name + '_' + this.Position;
+    this.check = document.getElementById(
+      this.Name + "_" + this.Position
+    ) as HTMLInputElement;
 
-        this.check = document.getElementById(this.Name + '_' + this.Position) as HTMLInputElement;
-
-        if (this.check != null)
-            this.check.checked = this.Checked;
-    }
+    if (this.check != null) this.check.checked = this.Checked;
+  }
 }
