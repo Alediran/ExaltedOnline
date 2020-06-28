8f4083055fa38943f59e10ff56f63e67da3d6a30
Alediran <55855331+Alediran@users.noreply.github.com>
2020-06-28 16:04:01 -0300

    Updated Angular Version

--- ExaltedOnlineWeb/ClientApp/src/app/core/data.model.ts
+++ ExaltedOnlineWeb/ClientApp/src/app/core/data.model.ts
@@ -1,44 +1,42 @@
-import { Injectable } from '@angular/core';
-import { Adapter } from './adapter';
+import { Injectable } from "@angular/core";
+import { Adapter } from "./adapter";
 
 export interface CharmModel {
-    message: String;
-    didError: Boolean;
-    errorMessage: String;
-    model: Charm[];
-    pageSize: Number;
-    pageNumber: Number;
-    itemsCount: Number;
-    pageCount: Number;
+  message: String;
+  didError: Boolean;
+  errorMessage: String;
+  model: Charm[];
+  pageSize: Number;
+  pageNumber: Number;
+  itemsCount: Number;
+  pageCount: Number;
 }
 
-export interface BoolModel {    
-    message: String;
-    didError: Boolean;
-    errorMessage: String;
-    model: Boolean;
+export interface BoolModel {
+  message: String;
+  didError: Boolean;
+  errorMessage: String;
+  model: Boolean;
 }
 
-export class Charm {
-    constructor(
-        public characterCharms: any[],
-        public charmCosts: any[],
-        public charmKeywords: any[],
-        public charmPrerequisites: any[],
-        public charmPrerequisitescharmPrerequisite: any[],
-        public charmTraits: any[],
-        public description: String,
-        public duration: any,
-        public durationId: Number,
-        public essence: Number,
-        public gameId: Number,
-        public id: Number,
-        public isCustomCharm: Boolean,
-        public title: String,
-        public treeTypeId: Number,
-        public type: any,
-        public typeId: Number
-    ) {}
+export interface Charm {
+  characterCharms: any[];
+  charmCosts: any[];
+  charmKeywords: any[];
+  charmPrerequisites: any[];
+  charmPrerequisitescharmPrerequisite: any[];
+  charmTraits: any[];
+  description: String;
+  duration: any;
+  durationId: Number;
+  essence: Number;
+  gameId: Number;
+  id: Number;
+  isCustomCharm: Boolean;
+  title: String;
+  treeTypeId: Number;
+  type: any;
+  typeId: Number;
 }
 
 //export class CharmModelAdapter implements Adapter<CharmModel>{
@@ -57,51 +55,13 @@ export class Charm {
 //    };
 //}
 
-export class CharmAdapter implements Adapter<Charm>{
-
-    adapt(item: any): Charm {
-        return new Charm(
-            item.characterCharms,
-            item.charmCosts,
-            item.charmKeywords,
-            item.charmPrerequisites,
-            item.charmPrerequisitescharmPrerequisite,
-            item.charmTraits,
-            item.description,
-            item.duration,
-            item.durationId,
-            item.essence,
-            item.gameId,
-            item.id,
-            item.isCustomCharm,
-            item.name,
-            item.treeTypeId,
-            item.type,
-            item.typeId
-        );
-    };
+export interface User {
+  password: string;
+  id?: number;
+  userName: string;
+  email: string;
+  passwordHash: string;
+  passwordSalt: string;
+  userRole: number;
+  signatureFirst: string;
 }
-
-export class User {
-
-    public password: string;
-    public id?: number;
-    public userName: string;
-    public email: string;
-    public passwordHash: string;
-    public passwordSalt: string;
-    public userRole: number;
-    public signatureFirst: string;
-
-    constructor() {
-        this.id = 0;
-        this.userName = '';
-        this.email = '';
-        this.passwordSalt = '';
-        this.passwordHash = '';
-        this.userRole = 1;
-        this.signatureFirst = '';
-
-    }
-}
-
